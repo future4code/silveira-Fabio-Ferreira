b4d7f4f71b94d76e8c5dcd7f9fe7254b1ef7dc09
FabioMatheus777 <fabioMatheusXLR8@gmail.com>
2022-04-08 20:21:32 -0300

    finalizando atividade

--- Modulo2/apis-labenusers/src/components/TelaCadastro.js
+++ Modulo2/apis-labenusers/src/components/TelaCadastro.js
@@ -1,5 +1,30 @@
 import axios from "axios";
 import React from "react";
+import styled from "styled-components"
+
+const Titulo = styled.h2`
+    font-size: 2rem;
+    color: lightcoral
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10px;
+    margin-top: 10px;
+`
+
+const InputBonito = styled.input`
+    width: 250px;
+    padding: 8px;
+    margin: 10px;
+`
+const BotaoBonito = styled.button`
+    height: 45px;
+    width: 150px;
+
+`
 
 export default class TelaCadastro extends React.Component{
     state = {
@@ -18,42 +43,44 @@ export default class TelaCadastro extends React.Component{
     cadastrarUsuario = () => {
         const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
         const body = {
-            name: this.cadastroNome,
-            email: this.cadastroEmail
+            name: this.state.nome,
+            email: this.state.email
         }
 
 
         axios
         .post(url, body, {
             headers:{
-            Authorization: "Fabio-Matheus-Silveira"
+            Authorization: "Fabio-Ferreira-Silveira"
         }
         })
         .then((res) => {
             alert("cadastro feito com sucesso!")
+            this.setState({nome:"", email:""})
         })
         .catch((err) => {
             alert(err.response.data.message)
         })
+        console.log(this.state)
     }
 
     render(){
         return(
-            <div>
-                <button onClick={this.props.irParaLista}>Ir para lista de Usuários</button>
-                <h2>TelaCadastro</h2>
-                <input
+            <Container>
+                <BotaoBonito onClick={this.props.irParaLista}>Ir para lista de Usuários</BotaoBonito>
+                <Titulo>TelaCadastro</Titulo>
+                <InputBonito
                 placeholder="nome"
-                value={this.state.name}
+                value={this.state.nome}
                 onChange={this.cadastroNome}
                 />
-                <input
+                <InputBonito
                 placeholder="E-mail"
                 value={this.state.email}
                 onChange={this.cadastroEmail}
                 />
                 <button onClick={this.cadastrarUsuario}>cadastrar</button>
-            </div>
+            </Container>
         )
     }
 }
