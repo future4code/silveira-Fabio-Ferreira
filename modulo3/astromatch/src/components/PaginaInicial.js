f6599f86e5d33245a4ee7682ecd5b6e801644ca3
FabioMatheus777 <fabiomatheusxlr8@gmail.com>
2022-04-22 20:28:15 -0300

    projeto finalizado

--- modulo3/astromatch/src/components/PaginaInicial.js
+++ modulo3/astromatch/src/components/PaginaInicial.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components';
+import {FaHeartBroken} from 'react-icons/fa'
+import {FaHeart} from 'react-icons/fa'
+import {BsPeopleFill} from 'react-icons/bs'
 
 const GlobalStyle = createGlobalStyle`
     body{
@@ -15,7 +18,8 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
-let Container = styled.div`
+
+const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -27,26 +31,155 @@ let Container = styled.div`
     background-color: #8a2be2;
 `
 
-let Foto = styled.img`
-    width: 90%;
-    height: 70%;
-    border: solid;
+
+const Foto = styled.img`
+    width: 350px;
+    height: 400px;
+    border-radius: 8px;
 `
+
+
+
+const Pessoa = styled.div`
+    height: 30%;
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.5), transparent);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    padding: 15px;
+    z-index: 2;
+    h2{
+    display: flex;
+    -webkit-box-align: baseline;
+    align-items: baseline;
+    }
+    p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+}
+`
+
+
+const Card = styled.div`
+    box-shadow: rgb(117 117 117 / 77%) 0px 2px 10px 0px;
+    position: relative;
+    border-radius: 5px;
+    overflow: hidden;
+    transition: all 0.5s ease 0s;
+    height: 400px;
+    animation: 0.5s ease 0s 1 normal forwards running none;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+`
+
+const DivBotaoEscolha = styled.div`
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    button.aceita{
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        color: #FF6347;
+        border: #FF6347;
+        :hover{
+            transform:scale(1.3);
+            transition-property: all;
+            transition-duration: 0.6;
+            transition-timing-function: ease;
+            color: white;
+            background-color: #FF6347;
+            box-shadow: #FFEFD5;
+            cursor: pointer;
+        }
+        position: relative;
+        :active{
+            transform: scale(1.1);
+            transition: all 0.2 ease;
+        }
+    }
+    button.recusa{
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        color: black;
+        border: black;
+        :hover{
+            transform:scale(1.3);
+            transition-property: all;
+            transition-duration: 0.6;
+            transition-timing-function: ease;
+            color: white;
+            background-color: black;
+            box-shadow: #FFEFD5;
+            cursor: pointer;
+        }
+        position: relative;
+        :active{
+            transform: scale(1.1);
+            transition: all 0.2 ease;
+        }
+    }
+`
+
+const Logo = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 80px;
+    width: 100%;
+    padding: 15px 0px;
+    img{
+        height:80%
+    }
+    button{
+        width: 60px;
+        height: 60px;
+        border-radius: 10%;
+        color: #EE82EE;
+        border: #EE82EE;
+        :hover{
+            transform:scale(1.2);
+            transition-property: all;
+            transition-duration: 0.6;
+            transition-timing-function: ease;
+            color: white;
+            background-color: #EE82EE;
+            box-shadow: #FFEFD5;
+            cursor: pointer;
+        }
+    }
+`
+
 
 const PaginaInicial = (props) => {
 
 const [perfil, setPerfil] = useState([])
 const [match, setMatch] = useState(false)
 
+
+
 const pegarTodosMatches = () => {
     axios.get(
-        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:fabio/person'
+        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fabio/person'
         )
         .then((res) => {
             setPerfil(res.data.profile)
         })
         .catch((err) => {
-            alert('algo de errado não está certo 🤔')
+            console.log('algo de errado não está certo 🤔')
         })
 }
 
@@ -62,10 +195,12 @@ const escolheMatch = () => {
 
     axios
     .post(
-        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person',
+        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fabio/choose-person',
          body)
     .then((res)=>{
-        console.log('bananinha')
+        console.log('tudo certo')
+        setMatch(res.data);
+        pegarTodosMatches();
     })
     .catch((err)=>{
         console.log('algo de errado n esta certo')
@@ -74,14 +209,22 @@ const escolheMatch = () => {
 
     return(
         <Container>
-            <button onClick={props.telaMatch} >matches</button>
-            <h2>titulo 🥰</h2>
-            <Foto src={perfil.photo} />
-            <h2>{perfil.name}</h2>
-            <p>{perfil.age}</p>
-            <p>{perfil.bio}</p>
-            <button>refuse</button>
-            <button onClick={() => escolheMatch()}>agree</button>
+            <Logo>
+                <button onClick={props.telaMatch} ><BsPeopleFill /></button>
+                <img src={'http://astro-match.com/images/logo_small.png'} />
+            </Logo>
+            <Card>
+                <Foto src={perfil.photo} />
+                <Pessoa>
+                    <h4>{perfil.name}</h4>
+                    <p>{perfil.age}</p>
+                    <p>{perfil.bio}</p>
+                </Pessoa>
+            </Card>
+            <DivBotaoEscolha>
+                <button className="recusa" onClick={() => pegarTodosMatches()}><FaHeartBroken /></button>
+                <button className="aceita" onClick={() => escolheMatch()}><FaHeart /></button>
+            </DivBotaoEscolha>
             <GlobalStyle/>
         </Container>
     )
--- modulo3/astromatch/src/components/TelaMatches.js
+++ modulo3/astromatch/src/components/TelaMatches.js
@@ -1,11 +1,132 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import styled from 'styled-components'
+import axios from "axios";
+import { createGlobalStyle } from 'styled-components';
+import MatchesList from './MatchesList'
+import {FaTrashAlt} from 'react-icons/fa'
+
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        display: flex;
+        justify-content: center;
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+        background-color: #808080;
+        padding-top:50px;
+    }
+`
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: solid;
+    border-color: #8a2be2;
+    border-radius: 8px;
+    height: 600px;
+    width: 400px;
+    background-color: #8a2be2;
+`
+
+const Logo = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 80px;
+    width: 100%;
+    padding: 15px 0px;
+    img{
+        height:80%
+    }
+    button{
+        width: 60px;
+        height: 60px;
+        border-radius: 10%;
+        color: black;
+        border: #EE82EE;
+        :hover{
+            transform:scale(1.2);
+            transition-property: all;
+            transition-duration: 0.6;
+            transition-timing-function: ease;
+            color: white;
+            background-color: #EE82EE;
+            box-shadow: #FFEFD5;
+            cursor: pointer;
+        }
+    }
+`
+
+const BotaoDelete = styled.button`
+    width: 60px;
+    height: 60px;
+    border-radius: 8px;
+    border: brown;
+    :hover{
+            transform:scale(1.2);
+            transition-property: all;
+            transition-duration: 0.6;
+            transition-timing-function: ease;
+            color: white;
+            background-color: #EE82EE;
+            box-shadow: #FFEFD5;
+            cursor: pointer;
+        }
+        :active{
+            transform: scale(1.1);
+            transition: all 0.2 ease;
+        }
+    position: relative;
+`
+
 
 const TelaMatches = (props) => {
+
+const [matchesEscolhidos, setMatchesEscolhidos] = useState ([]);
+
+
+
+useEffect(()=>{
+    axios.get(
+        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fabio/matches'
+        )
+    .then((res)=>{
+        console.log('deu certo')
+        setMatchesEscolhidos(res.data.matches)
+    })
+    .catch((err)=>{        
+        console.log('deu errado')
+    })
+}, [])
+
+const apagaGeral = () => {
+    axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fabio/clear')
+    .then(()=>{
+        alert('sua lista foi apagada 😆')
+    })
+    .catch(()=>{
+        alert('to entendendo nada 🥴')
+    })
+}
+
     return(
-        <div>
-            <h2>Fala tu</h2>
-            <button onClick={props.selectMatch}>Tela Inicial</button>
-        </div>
+        <Container>
+            <Logo>
+                <button onClick={props.selectMatch}>Tela Inicial</button>
+                <img src={'http://astro-match.com/images/logo_small.png'} />
+            </Logo>  
+                        
+                  {matchesEscolhidos.map((perfil) => {
+                      return <MatchesList perfil={perfil} />
+                  })}            
+                    
+            <BotaoDelete onClick={apagaGeral}><FaTrashAlt /></BotaoDelete>
+            <GlobalStyle/>
+        </Container>
     )
 }
 
