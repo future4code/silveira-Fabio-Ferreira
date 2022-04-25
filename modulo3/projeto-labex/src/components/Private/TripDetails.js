83468408f35d50e8cb64a1ec15b118a3052d4c41
FabioMatheus777 <fabiomatheusxlr8@gmail.com>
2022-04-25 18:45:45 -0300

    primeira entrega do projeto

---
+++ modulo3/projeto-labex/src/components/Private/TripDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TripDetails = () => {
+  return (
+    <div>
+      eo
+    </div>
+  );
+}
---
+++ modulo3/projeto-labex/src/components/Public/ApplicationForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ApplicationForm = () => {
+  return (
+    <div>
+      eo
+    </div>
+  );
+}
