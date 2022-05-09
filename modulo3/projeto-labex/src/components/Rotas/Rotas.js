8a25a792b27384d0008b8ba52de5fe8ac8ea4c5c
FabioMatheus777 <fabiomatheusxlr8@gmail.com>
2022-05-09 19:10:11 -0300

    inicio

--- modulo3/projeto-labex/src/components/Rotas/Rotas.js
+++ modulo3/projeto-labex/src/components/Rotas/Rotas.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Home } from '../components/Home/Home'
-import { AdminHome } from '../components/Private/AdminHome'
-import { CreateTrip } from '../components/Private/CreateTrip'
-import { Login } from '../components/Private/Login'
-import { TripDetails } from '../components/Private/TripDetails'
-import { ApplicationForm } from '../components/Public/ApplicationForm'
-import { ListTrips } from '../components/Public/ListTrips'
+import { Home } from '../Home/Home'
+import { AdminHome } from '../Private/AdminHome'
+import { CreateTrip } from '../Private/CreateTrip'
+import { Login } from '../Private/Login'
+import { TripDetails } from '../Private/TripDetails'
+import { ApplicationForm } from '../Public/ApplicationForm'
+import { ListTrips } from '../Public/ListTrips'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
