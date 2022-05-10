fbff2849b90dd7c9c09fd313bf8ab7ddda44c731
FabioMatheus777 <fabiomatheusxlr8@gmail.com>
2022-05-10 19:24:13 -0300

    rotas

---
+++ modulo3/labeddit/labeddit/src/pages/SingupPage/SingupPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SingupPage = () => {
+    return(
+        <div>
+            <h1>Singup Page</h1>
+        </div>
+    )
+}
---
+++ modulo3/labeddit/labeddit/src/routes/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ErrorPage } from "../pages/ErrorPage/ErrorPage";
+import { FeedPage } from "../pages/FeedPage/FeedPage";
+import { LoginPage } from "../pages/LoginPage/LoginPage";
+import { PostDetailPage } from "../pages/PostDetailPage/PostDetailPage";
+import { SingupPage } from "../pages/SingupPage/SingupPage";
+
+export const Router = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<FeedPage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/cadastro' element={<SingupPage />} />
+                <Route path='/detalhes/:id' element={<PostDetailPage />} />
+                <Route path='*' element={<ErrorPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
